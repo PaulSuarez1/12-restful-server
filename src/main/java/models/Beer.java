27ed986037487433ace1eea8210daf8938529c14
Paul Suarez <paulsblogging.com@gmail.com>
2018-07-25 13:49:23 -0700

    finished for now

---
+++ src/main/java/models/Beer.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Beer {
+    private static int ID_COUNT = 12_543;
+
+    public int id;
+    public String name;
+    public float abv;
+
+    public Beer(String name, float abv) {
+        
+    }
+}
