df5b83aade9c8e998d05dc0c1ecdeb04b56b5035
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-04 17:44:21 -0700

    ok updates are working

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
+        System.out.println("");
         System.out.println("http://localhost:8080/beerlist/");
+        System.out.println("");
     }
 }
